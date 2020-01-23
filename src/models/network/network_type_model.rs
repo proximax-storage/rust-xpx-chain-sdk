486b8d4d462ae2ca43999bb1e26dc094c276d982
slackve <eleazartovar123@gmail.com>
2020-01-23 17:37:21 -0500

    add model networkType model

--- src/models/network/network_type_model.rs
+++ src/models/network/network_type_model.rs
@@ -1,18 +1,62 @@
-#[derive(Debug, PartialEq)]
-pub struct NetworkType {
-    /// The name of the network.
-    #[serde(rename = "name")]
-    pub name: String,
-    /// A short text describing the network.
-    #[serde(rename = "description")]
-    pub description: String,
+use models::errors_model::InternalError;
+use models::errors_model::ModelError;
+
+/// MIJIN private network identifier. Decimal value = 96.
+pub const MIJIN: NetworkType = NetworkType { _value: 0x60 };
+
+/// MIJIN_TEST private test network identifier. Decimal value = 144.
+pub const MIJIN_TEST: NetworkType = NetworkType { _value: 0x90 };
+
+/// The PUBLIC test network identifier. Decimal value = 184.
+pub const PUBLIC: NetworkType = NetworkType { _value: 0xb8 };
+
+/// The PUBLIC_TEST test network identifier. Decimal value = 168.
+pub const PUBLIC_TEST: NetworkType = NetworkType { _value: 0xa8 };
+
+/// The PRIVATE test network identifier. Decimal value = 200.
+pub const PRIVATE: NetworkType = NetworkType { _value: 0xc8 };
+
+/// The PRIVATE_TEST test network identifier. Decimal value = 176.
+pub const PRIVATE_TEST: NetworkType = NetworkType { _value: 0xb0 };
+
+pub const ALIAS_ADDRESS: NetworkType = NetworkType { _value: 0x91 };
+
+pub const NOT_SUPPORTED_NET: NetworkType = NetworkType { _value: 0 };
+
+pub struct NetworkType { _value: u8 }
+
+impl NetworkType {
+    pub fn from_string(network_type: &str) -> Result<NetworkType, ModelError> {
+        match network_type {
+            "MIJIN" => Ok(MIJIN),
+            "MIJIN_TEST" => Ok(MIJIN_TEST),
+            "PUBLIC" => Ok(PUBLIC),
+            "PUBLIC_TEST" => Ok(PUBLIC_TEST),
+            "PRIVATE" => Ok(PRIVATE),
+            "PRIVATE_TEST" => Ok(PRIVATE_TEST),
+            "ALIAS_ADDRESS" => Ok(ALIAS_ADDRESS),
+            _ => Err(ModelError(InternalError::NetworkTypeError))
+        }
+    }
 }
 
 impl NetworkType {
-    pub fn new(name: String, description: String) -> NetworkTypeDto {
-        NetworkTypeDto {
-            name,
-            description,
+    pub fn from_int(network_type: u8) -> Result<NetworkType, ModelError> {
+        match network_type {
+            0x60 => Ok(MIJIN),
+            0x90 => Ok(MIJIN_TEST),
+            0xb8 => Ok(PUBLIC),
+            0xa8 => Ok(PUBLIC_TEST),
+            0xc8 => Ok(PRIVATE),
+            0xb0 => Ok(PRIVATE_TEST),
+            0x91 => Ok(ALIAS_ADDRESS),
+            _ => Err(ModelError(InternalError::NetworkTypeError))
         }
+    }
+}
+
+impl std::fmt::Display for NetworkType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "NetworkType: {}", self._value)
     }
 }
