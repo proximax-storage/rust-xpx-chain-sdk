ba3a66fbd75afe91525d0c494723293c65a6e441
slackve <eleazartovar123@gmail.com>
2020-02-20 17:01:19 -0500

    update get_transaction example

--- examples/get_transactions.rs
+++ examples/get_transactions.rs
@@ -11,12 +11,29 @@ async fn main() {
 
     let client = SiriusClient::new(node, Client::new());
 
-    let status = client.transaction.get_transaction_status(
+    let transaction_status = client.clone().transaction.get_transaction_status(
         "5BD1AE427B10E50902DE8037B7A551104727732B2C97A08A3DD19342714337A5"
     ).await;
 
-    match status {
-        Ok(resp) => println!("{}", resp),
+    match transaction_status {
+        Ok(status) => println!("{}", status),
         Err(err) => eprintln!("RESP_ERROR: {:?}", err),
+    }
+
+    let statues_vec = vec![
+        "5BD1AE427B10E50902DE8037B7A551104727732B2C97A08A3DD19342714337A5",
+        "c088f96252ab76ac73b4367de2c8c6d7dfaf33ea3e7120d867888632862eb514"
+    ];
+
+    let transactions_statuses = client.transaction.get_transactions_statuses(
+        statues_vec).await;
+
+    match transactions_statuses {
+        Ok(statuses) => {
+            for status in statuses {
+                println!("{}", status)
+            }
+        }
+        Err(err) => eprintln!("{:?}", err),
     }
 }
