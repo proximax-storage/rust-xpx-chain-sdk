f61dba38b4dfa5f077b0015a92d1bc2f6ffe1d21
slackve <eleazartovar123@gmail.com>
2020-03-04 14:28:29 -0500

    update example

--- examples/api_account.rs
+++ examples/api_account.rs
@@ -1,18 +1,15 @@
-#![deny(warnings)]
-#![warn(rust_2018_idioms)]
-
 use hyper::Client;
-
 use xpx_chain_sdk::apis::sirius_client::SiriusClient;
+
+const NODE_URL: &str = "http://bctestnetswap.xpxsirius.io:3000";
+const PUBLIC_KEY: &str = "93C3B9075649F59BD88573ADC55B8915B12390A47C76F0C45F362ED0800BE237";
 
 #[tokio::main]
 async fn main() {
-    let node = "http://bctestnetswap.xpxsirius.io:3000";
 
-    let client = SiriusClient::new(node, Client::new());
+    let client = SiriusClient::new(NODE_URL, Client::new());
 
-    let account_info = client.account.get_account_info(
-        "93C3B9075649F59BD88573ADC55B8915B12390A47C76F0C45F362ED0800BE237").await;
+    let account_info = client.account.get_account_info(PUBLIC_KEY ).await;
 
     match account_info {
         Ok(resp) => println!("{}", resp),
