026ab09b9bad38abd2c03c60695d82d12ca64009
slackve <eleazartovar123@gmail.com>
2020-03-04 15:55:00 -0500

    add documentation of ChainRoutes

--- src/apis/chain_routes_api.rs
+++ src/apis/chain_routes_api.rs
@@ -16,6 +16,8 @@ use crate::{
 
 use super::{request as __internal_request, Result};
 
+/// Chain ApiClient routes.
+///
 #[derive(Clone)]
 pub struct ChainRoutes<C: Connect> {
     client: Arc<ApiClient<C>>,
@@ -29,9 +31,40 @@ impl<C: Connect> ChainRoutes<C> {
     }
 }
 
+/// Chain related endpoints.
+///
 impl<C: Connect> ChainRoutes<C> where
-    C: Clone + Send + Sync + Debug + 'static
+    C: Clone + Send + Sync + 'static
 {
+
+    /// Get the current height of the chain
+    ///
+    /// # Example
+    ///
+    /// ```
+    ///use hyper::Client;
+    ///use xpx_chain_sdk::apis::sirius_client::SiriusClient;
+    ///
+    ///const NODE_URL: &str = "http://bctestnetswap.xpxsirius.io:3000";
+    ///
+    ///#[tokio::main]
+    ///async fn main() {
+    ///
+    ///    let client = SiriusClient::new(node, Client::new());
+    ///
+    ///    let chain_height = client.chain.get_blockchain_height().await;
+    ///
+    ///    match chain_height {
+    ///        Ok(resp_info) => println!("{}", resp_info),
+    ///        Err(err) => eprintln!("{:?}", err),
+    ///    }
+    ///}
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// Returns a Future `Result` whose okay value is the current height of the blockchain or
+    /// whose error value is an `Error<Value>` describing the error that occurred.
     pub async fn get_blockchain_height(self) -> Result<HeightInfo> {
         let req = __internal_request::Request::new(
             Method::GET,
@@ -42,6 +75,41 @@ impl<C: Connect> ChainRoutes<C> where
         Ok(dto?.to_struct())
     }
 
+    /// Get the current score of the chain.
+    ///
+    /// Gets the current score of the blockchain. The higher the score, the better the chain.
+    /// During synchronization, nodes try to get the best blockchain in the network.
+    /// The score for a block is derived from its difficulty and the time (in seconds)
+    /// that has elapsed since the last block:
+    /// * block score = difficulty − time elapsed since last block
+    ///
+    /// # Example
+    ///
+    /// ```
+    ///use hyper::Client;
+    ///use xpx_chain_sdk::apis::sirius_client::SiriusClient;
+    ///
+    ///const NODE_URL: &str = "http://bctestnetswap.xpxsirius.io:3000";
+    ///
+    ///#[tokio::main]
+    ///async fn main() {
+    ///
+    ///    let client = SiriusClient::new(node, Client::new());
+    ///
+    ///    let chain_score = client.chain.get_blockchain_score().await;
+    ///
+    ///    match chain_score {
+    ///        Ok(resp_info) => println!("{}", resp_info),
+    ///        Err(err) => eprintln!("{:?}", err),
+    ///    }
+    ///}
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// Returns a Future `Result` whose okay value is [BlockchainScore] or
+    /// whose error value is an `Error<Value>` describing the error that occurred.
+    
     pub async fn get_blockchain_score(self) -> Result<BlockchainScore> {
         let req = __internal_request::Request::new(
             Method::GET,
