60d422b72ed8670eb4ee3e9cc9f2af99f6069424
slackve <eleazartovar123@gmail.com>
2020-03-05 09:59:51 -0500

    add documentation or mosaic routes

--- src/apis/mosaic_routes_api.rs
+++ src/apis/mosaic_routes_api.rs
@@ -82,6 +82,48 @@ impl<C: Connect> MosaicRoutes<C> where
         Ok(dto?.to_struct()?)
     }
 
+    /// Gets an vector of [MosaicInfo] definition.
+    ///
+    /// Get mosaics information for an vector of mosaics
+    ///
+    /// # Inputs
+    ///
+    /// * `mosaics_id` =    The vector of mosaic identifiers.
+    ///
+    /// # Example
+    ///
+    /// ```
+    ///use hyper::Client;
+    ///use xpx_chain_sdk::apis::sirius_client::SiriusClient;
+    ///use xpx_chain_sdk::models::mosaic::MosaicId;
+    ///
+    ///const NODE_URL: &str = "http://bctestnet1.brimstone.xpxsirius.io:3000";
+    ///
+    ///#[tokio::main]
+    ///async fn main() {
+    ///
+    /// let client = SiriusClient::new(NODE_URL, Client::new());
+    ///
+    ///    let mosaic_id_a = MosaicId::from_hex("3C520B7CEB2F7099").unwrap();
+    ///    let mosaic_id_b = MosaicId::from_hex("6208AE4D56451357").unwrap();
+    ///
+    ///    let mosaics_info = client.mosaic.get_mosaics_info(vec![mosaic_id_a, mosaic_id_b]).await;
+    ///
+    ///    match mosaics_info {
+    ///        Ok(resp) => {
+    ///            for mosaic in resp {
+    ///                println!("{}", mosaic)
+    ///            }
+    ///        }
+    ///        Err(err) => panic!("{:?}", err),
+    ///    }
+    ///}
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// Returns a Future `Result` whose okay value is an verctor of [MosaicInfo] or
+    /// whose error value is an `Error<Value>` describing the error that occurred.
     pub async fn get_mosaics_info(self, mosaic_ids: Vec<MosaicId>) -> Result<Vec<MosaicInfo>> {
         valid_vec_len(&mosaic_ids, ERR_EMPTY_MOSAIC_IDS)?;
 
@@ -104,6 +146,46 @@ impl<C: Connect> MosaicRoutes<C> where
         Ok(mosaics_info)
     }
 
+    /// Get readable names for a set of mosaics
+    ///
+    /// # Inputs
+    ///
+    /// * `mosaics_id` =    The vector of mosaic identifiers.
+    ///
+    /// # Example
+    ///
+    /// ```
+    ///use hyper::Client;
+    ///use xpx_chain_sdk::apis::sirius_client::SiriusClient;
+    ///use xpx_chain_sdk::models::mosaic::MosaicId;
+    ///
+    ///const NODE_URL: &str = "http://bctestnet1.brimstone.xpxsirius.io:3000";
+    ///
+    ///#[tokio::main]
+    ///async fn main() {
+    ///
+    /// let client = SiriusClient::new(NODE_URL, Client::new());
+    ///
+    ///    let mosaic_id_a = MosaicId::from_hex("3C520B7CEB2F7099").unwrap();
+    ///    let mosaic_id_b = MosaicId::from_hex("6208AE4D56451357").unwrap();
+    ///
+    ///    let mosaics_names = client.mosaic.get_mosaics_names(vec![mosaic_id_a, mosaic_id_b]).await;
+    ///
+    ///    match mosaics_names {
+    ///        Ok(resp) => {
+    ///            for mosaic in resp {
+    ///                println!("{}", mosaic)
+    ///            }
+    ///        }
+    ///        Err(err) => panic!("{:?}", err),
+    ///    }
+    ///}
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// Returns a Future `Result` whose okay value is an friendly names for mosaics or
+    /// whose error value is an `Error<Value>` describing the error that occurred.
     pub async fn get_mosaics_names(self, mosaic_ids: Vec<MosaicId>) -> Result<Vec<MosaicNames>> {
         valid_vec_len(&mosaic_ids, ERR_EMPTY_MOSAIC_IDS)?;
 
