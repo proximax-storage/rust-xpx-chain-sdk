cb097f0ac0ff39254ba30f6fb7b20b267c76b7bd
slackve <eleazartovar123@gmail.com>
2020-03-26 23:23:45 -0500

    update example

--- examples/api_transactions.rs
+++ examples/api_transactions.rs
@@ -4,6 +4,7 @@
 use hyper::Client;
 
 use xpx_chain_sdk::sirius_client::SiriusClient;
+use xpx_chain_sdk::transaction::TransferTransaction;
 
 const NODE_URL: &str = "http://bctestnet1.brimstone.xpxsirius.io:3000";
 
@@ -14,44 +15,44 @@ async fn main() {
         Ok(resp) => resp,
         Err(err) => panic!("{}", err),
     };
-//
-//    let transaction_status = client.clone().transaction.get_transaction_status(
-//        "C7EE3B9FB368AAA447A02E49E2F3D1CA3190300E4CDB9E5C6370C927C64682E1").await;
-//    match transaction_status {
-//        Ok(status) => println!("{}", status),
-//        Err(err) => eprintln!("{}", err),
-//    }
-//
-//    let transactions_ids = vec![
-//        "E55037A661EE69C5F45CD3F40744F0923DD5B827F50BAF9AAF61DF649DC7B1D9",
-//        "5EC5C0E766B3DF81FBAD0E4FD794828002763905FEDC47208520E90FBED783B4"
-//    ];
-//
-//    let transactions_statuses = client.clone().transaction.get_transactions_statuses(
-//        transactions_ids.clone()).await;
-//    match transactions_statuses {
-//        Ok(statuses) => {
-//            statuses.iter().for_each(|status| {
-//                println!("{}", status)
-//            })
-//        }
-//        Err(err) => eprintln!("{}", err),
-//    }
+
+    let transaction_status = client.clone().transaction.get_transaction_status(
+        "C7EE3B9FB368AAA447A02E49E2F3D1CA3190300E4CDB9E5C6370C927C64682E1").await;
+    match transaction_status {
+        Ok(status) => println!("{}", status),
+        Err(err) => eprintln!("{}", err),
+    }
+
+    let transactions_ids = vec![
+        "E55037A661EE69C5F45CD3F40744F0923DD5B827F50BAF9AAF61DF649DC7B1D9",
+        "5EC5C0E766B3DF81FBAD0E4FD794828002763905FEDC47208520E90FBED783B4"
+    ];
+
+    let transactions_statuses = client.clone().transaction.get_transactions_statuses(
+        transactions_ids.clone()).await;
+    match transactions_statuses {
+        Ok(statuses) => {
+            statuses.iter().for_each(|status| {
+                println!("{}", status)
+            })
+        }
+        Err(err) => eprintln!("{}", err),
+    }
 
     let transaction = client.clone().transaction.get_transaction(
-        "0BA027750F12FEC3228A8D4E166D2A817C0550089DA3ED2EF4B3F28BCD7A0133").await;
+        "C7EE3B9FB368AAA447A02E49E2F3D1CA3190300E4CDB9E5C6370C927C64682E1").await;
     match transaction {
         Ok(tx) => println!("{}", tx),
         Err(err) => eprintln!("{}", err),
     }
-//
-//    let transactions = client.clone().transaction.get_transactions(transactions_ids).await;
-//    match transactions {
-//        Ok(txs) => {
-//            txs.into_iter().for_each( |tx_info| {
-//                println!("{}", tx_info)
-//            })
-//        }
-//        Err(err) => eprintln!("{}", err),
-//    }
+
+    let transactions = client.clone().transaction.get_transactions(transactions_ids).await;
+    match transactions {
+        Ok(txs) => {
+            txs.into_iter().for_each( |tx_info| {
+                println!("{}", tx_info)
+            })
+        }
+        Err(err) => eprintln!("{}", err),
+    }
 }
